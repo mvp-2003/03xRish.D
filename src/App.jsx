79da51a3a9ce15850586458c57a37406d20f3fa4
Rishabh <113057571+mvp-2003@users.noreply.github.com>
2024-01-05 20:25:28 +0000

    Changed Images and About. Portfolio needs to be changed anddeployed.

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Rishabh Das",
+  title: "Software Engineer / Cloud / IT & Cybersecurity",
+  email: "rishabh.babi@gmail.com",
+  gitHub: "mvp-2003",
+  instagram: "__d_mvp___",
+  linkedIn: "Rishabh Das",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "@d_mvpRishabh",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
