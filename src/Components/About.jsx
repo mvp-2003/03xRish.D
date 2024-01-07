17a3e509e1c63e394ab3838ae6c410f8c6bf250a
Rishabh <113057571+mvp-2003@users.noreply.github.com>
2024-01-07 06:35:59 +0000

    chaging PR

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -70,7 +70,6 @@ const About = () => {
             fontSize: "1.25rem",
             margin: "2rem 3rem",
             gap: "3rem",
-            color: "white",
           }}
         >
           {skillsList.map((skill) => (
